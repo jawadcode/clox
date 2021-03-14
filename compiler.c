e77d78c9cdedfa5907c12f0ea45a6b0aec64c5d8
Jawad W. Ahmed <Jawad.W.Ahmed@gmail.com>
2021-03-14 22:03:51 +0000

    16.2 A Token at a Time

--- compiler.c
+++ compiler.c
@@ -7,4 +7,29 @@
 void compile(const char *source)
 {
 	initScanner(source);
+	// Little trick to make it print the first line number
+	int line = -1;
+
+	for (;;)
+	{
+		Token token = scanToken();
+		// Print line number ("token.line") if it differs with the previous ("line")
+		// Otherwise, print "   | "
+		if (token.line != line)
+		{
+			printf("%4d ", token.line);
+			line = token.line;
+		}
+		else
+		{
+			printf("   | ");
+		}
+
+		// Print token information
+		printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+		// End Of File
+		if (token.type == TOKEN_EOF)
+			break;
+	}
 }
