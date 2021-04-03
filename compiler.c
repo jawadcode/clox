b61e76d9b7e75ebc48b3cdc8ea21312297ae4b82
jawadcode <jawad.w.ahmed@gmail.com>
2021-04-03 16:23:27 +0100

    22.3 Declaring Local Variables

--- compiler.c
+++ compiler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -224,6 +225,14 @@ static void beginScope()
 static void endScope()
 {
 	current->scopeDepth--;
+
+	while (current->localCount > 0 &&
+				 current->locals[current->localCount - 1].depth >
+						 current->scopeDepth)
+	{
+		emitByte(OP_POP);
+		current->localCount--;
+	}
 }
 
 // Forward declarations
@@ -246,15 +255,63 @@ static uint8_t identifierConstant(Token *name)
 	return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
 }
 
+static void addLocal(Token name)
+{
+	if (current->localCount == UINT8_COUNT)
+	{
+		error("Too many local variables in function");
+		return;
+	}
+	// "localCount"'s value is used before incrementing
+
+	Local *local = &current->locals[current->localCount++];
+	local->name = name;
+	local->depth = current->scopeDepth;
+}
+
+static bool identifiersEqual(Token *a, Token *b)
+{
+	if (a->length != b->length)
+		return false;
+	return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static void declareVariable()
+{
+	if (current->scopeDepth == 0)
+		return;
+
+	Token *name = &parser.previous;
+	for (int i = current->localCount - 1; i >= 0; i--)
+	{
+		Local *local = &current->locals[i];
+		if (local->depth != -1 && local->depth < current->scopeDepth)
+			break;
+
+		if (identifiersEqual(name, &local->name))
+			error("Variable already exists in current scope");
+	}
+
+	addLocal(*name);
+}
+
 // Requires next token to be identifier and then returns index of constant in table from "identifierConstant()"
 static uint8_t parseVariable(const char *errorMessage)
 {
 	consume(TOKEN_IDENTIFIER, errorMessage);
+
+	declareVariable();
+	if (current->scopeDepth > 0)
+		return 0;
+
 	return identifierConstant(&parser.previous);
 }
 
 static void defineVariable(uint8_t global)
 {
+	if (current->scopeDepth > 0)
+		return;
+
 	emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
